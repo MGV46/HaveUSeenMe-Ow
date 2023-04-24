77062f452f2014950c24a1efc3417c662725acdf
Cesar Vera <matveraand@gmail.com>
2023-04-23 23:39:17 -0400

    cambios necesarios para hacer commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import ProfilePagePet from "scenes/profilePagePet";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -27,6 +28,10 @@ function App() {
             <Route
               path="/profile/:userId"
               element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+             <Route
+              path="/profilePet/:petId"
+              element={isAuth ? <ProfilePagePet /> : <Navigate to="/" />}
             />
           </Routes>
         </ThemeProvider>
